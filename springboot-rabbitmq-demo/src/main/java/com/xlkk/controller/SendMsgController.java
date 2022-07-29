273f8d575e8bc10c94f86c361adcf62fd94605d6
xlkk <60306671+xlkkqaq@users.noreply.github.com>
2022-07-29 14:29:37 +0800

    feat:新增延时队列发送消息-生产者 :new:

--- springboot-rabbitmq-demo/src/main/java/com/xlkk/controller/SendMsgController.java
+++ springboot-rabbitmq-demo/src/main/java/com/xlkk/controller/SendMsgController.java
@@ -1,5 +1,6 @@
 package com.xlkk.controller;
 
+import com.xlkk.config.DelayedQueueConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,11 @@ public class SendMsgController {
         rabbitTemplate.convertAndSend("X","XB","消息来自ttl为40s的队列："+message);
     }
 
+    /**
+     * 自定义时长-发送过期消息
+     * @param message
+     * @param ttlTime
+     */
     @GetMapping("/sendExpirationMsg/{message}/{ttlTime}")
     public void sendMsg(@PathVariable String message,@PathVariable String ttlTime){
         log.info("当前时间{},发送一条消息给一个时长是{}ms的TTL队列QC:{}",new Date().toString(),ttlTime,message);
@@ -41,4 +47,24 @@ public class SendMsgController {
             return msg;
         });
     }
+
+    /**
+     * 基于自定义交换机-延迟发送消息
+     * @param message
+     * @param delayedTime
+     */
+    @GetMapping("/sendDelayedMsg/{message}/{delayedTime}")
+    public void delayedSendMsg(@PathVariable String message,@PathVariable Integer delayedTime){
+        log.info("当前时间{},发送一条时长是{}ms的消息给延迟队列delayedqueue:{}",new Date().toString(),delayedTime,message);
+        rabbitTemplate.convertAndSend(DelayedQueueConfig.DELAYED_EXCHANGE_NAME,
+                DelayedQueueConfig.DELAYED_ROUTING_KEY,
+                "发给C的msg"+message,
+                (msg)->{
+            //设置发送消息的延时时长 ms
+            msg.getMessageProperties().setDelay(delayedTime);
+            return msg;
+        });
+    }
+
 }
+
