57f0582f54e88d93fd5d08d2db387e6727346f74
xlkk <60306671+xlkkqaq@users.noreply.github.com>
2022-07-29 16:35:50 +0800

    feat:新增生产者 :new:

---
+++ springboot-rabbitmq-demo/src/main/java/com/xlkk/controller/ProduceController.java
@@ -0,0 +1,34 @@
+package com.xlkk.controller;
+
+import com.xlkk.config.ConfirmConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xlkk
+ * @date 2022/7/29 0029 15:50
+ * @Description: 生产者
+ */
+@Slf4j
+@RestController
+@RequestMapping("/confirm")
+public class ProduceController {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/sendMsg/{message}")
+    public void sendMessage(@PathVariable String message){
+        rabbitTemplate.convertAndSend(
+                ConfirmConfig.CONFIRM_EXCHANGE_NAME,
+                ConfirmConfig.ROUTING_KEY,
+                message);
+        log.info("发送消息:{}",message);
+
+    }
+
+}
