f0191d92171629d330223087c2432db2a4973c01
xlkk <60306671+xlkkqaq@users.noreply.github.com>
2022-07-29 14:28:41 +0800

    :art: refactor

--- springboot-rabbitmq-demo/src/main/java/com/xlkk/consumer/DeadLetterConsumer.java
+++ springboot-rabbitmq-demo/src/main/java/com/xlkk/consumer/DeadLetterConsumer.java
@@ -11,7 +11,7 @@ import java.util.Date;
 
 /**
  * @author xlkk
- * @date 2022/7/28 0028 16:29
+ * date  2022/7/28 0028 16:29
  * 队列TTL的消费者
  */
 @Slf4j
@@ -21,8 +21,5 @@ public class DeadLetterConsumer {
     public void receiveD(Message message, Channel channel) throws UnsupportedEncodingException {
         String msg =new String(message.getBody(),"UTF-8");
         log.info("当前时间：{},收到死信队列的消息：{}",new Date().toString(),msg);
-
     }
-
-
 }
