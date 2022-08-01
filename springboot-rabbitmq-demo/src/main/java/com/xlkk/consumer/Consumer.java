2c71277a8f8a548003a3790b82cf8ef3612a3795
xlkk <60306671+xlkkqaq@users.noreply.github.com>
2022-08-01 22:39:22 +0800

    refactor :art:

--- springboot-rabbitmq-demo/src/main/java/com/xlkk/consumer/Consumer.java
+++ springboot-rabbitmq-demo/src/main/java/com/xlkk/consumer/Consumer.java
@@ -5,8 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
 /**
